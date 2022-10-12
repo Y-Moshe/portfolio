16ed514e8f5d6f75f1cf21cbce5a650d32969012
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-12 13:44:23 +0300

    proj service

---
+++ js/services/proj.service.js
@@ -0,0 +1,15 @@
+var gProjs = [
+  {
+    id: "sokoban",
+    name: "Sokoban",
+    title: "Better push those boxes",
+    desc: "lorem ipsum lorem ipsum lorem ipsum",
+    url: "projs/sokoban",
+    publishedAt: 1448693940000,
+    labels: ["Matrixes", "keyboard events"]
+  }
+]
+
+function getProjects() {
+  return gProjs
+}
