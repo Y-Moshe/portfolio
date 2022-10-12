4572086ebcce4bae12a4a4d360d3a074e0c9a62e
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-12 17:17:34 +0300

    add todo-gallery project

--- js/services/proj.service.js
+++ js/services/proj.service.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var gProjs = [
   {
     id: 'minesweeper',
@@ -5,8 +7,19 @@ var gProjs = [
     title: 'Minesweeper Game',
     desc: 'Minesweeper game build with html, css and javascript with a great features.',
     url: 'https://y-moshe.github.io/Minesweeper/',
+    githubUrl: 'https://github.com/Y-Moshe/Minesweeper',
     publishedAt: 1663770417000,
     labels: ['HTML', 'CSS', 'JS']
+  },
+  {
+    id: 'todo-gallery',
+    name: 'Todo Gallery',
+    title: 'Photos Gallery',
+    desc: 'Gallery application build with React.js uses unsplash api, a todo app to practice react.',
+    url: 'https://ym-todo-gallery.netlify.app/',
+    githubUrl: 'https://github.com/Y-Moshe/todo-gallery',
+    publishedAt: 1624477465000,
+    labels: ['HTML', 'CSS', 'JS', 'React', 'restfulAPI']
   },
   {
     id: 'fxp-generator',
@@ -14,6 +27,7 @@ var gProjs = [
     title: 'A Generate tool for Fxp.co.il common tasks.',
     desc: 'Simple BBCode string generator tool to quickly generate a common "Templates" to use in fxp.co.il posts.',
     url: 'https://ym-fxp-generator.netlify.app/',
+    githubUrl: 'https://github.com/Y-Moshe/fxp-generator',
     publishedAt: 1610278837000,
     labels: ['HTML', 'CSS', 'JS', 'React', 'Material-UI']
   }
