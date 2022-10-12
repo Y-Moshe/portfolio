eb9dee2a6cbaa57d825a685809a04a50886374c9
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-12 13:45:15 +0300

    Dynamiclly projects rendering

--- js/services/proj.service.js
+++ js/services/proj.service.js
@@ -1,12 +1,12 @@
 var gProjs = [
   {
-    id: "sokoban",
-    name: "Sokoban",
-    title: "Better push those boxes",
-    desc: "lorem ipsum lorem ipsum lorem ipsum",
-    url: "projs/sokoban",
-    publishedAt: 1448693940000,
-    labels: ["Matrixes", "keyboard events"]
+    id: "minesweeper",
+    name: "Minesweeper",
+    title: "Minesweeper game build with html, css and javascript.",
+    desc: "Minesweeper game build with html, css and javascript.",
+    url: "https://y-moshe.github.io/Minesweeper/",
+    publishedAt: 1663770417000,
+    labels: ['HTML', 'CSS', 'JS']
   }
 ]
 
