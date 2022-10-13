b46712b79244565608323cd6deff95f6ba17fafc
Y-Moshe <moshe.nehemiah@gmail.com>
2022-10-13 08:18:44 +0300

    customize theme

--- js/main.js
+++ js/main.js
@@ -31,19 +31,21 @@ function renderSocials(socials) {
 
 function renderProjects(projects) {
   const projsHtml = projects.map(project => `
-    <div class="col-md-4 col-sm-6 portfolio-item">
-      <a onclick="onProjClick('${project.id}')" class="portfolio-link" data-toggle="modal" href="#portfolioModal">
-        <div class="portfolio-hover">
-          <div class="portfolio-hover-content">
-            <i class="fa fa-plus fa-3x"></i>
+    <div class="col-md-4 col-sm-6">
+      <div class="portfolio-item d-flex flex-column">
+        <a onclick="onProjClick('${project.id}')" class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+          <div class="portfolio-hover">
+            <div class="portfolio-hover-content">
+              <i class="fa fa-plus fa-3x"></i>
+            </div>
           </div>
+          <img class="img-fluid" src="img/portfolio/${project.id}.png" alt="${project.name}">
+        </a>
+        <div class="portfolio-caption">
+          <h4>${project.name}</h4>
+          <p class="text-muted">${project.title}</p>
+          ${getProjLabels(project)}
         </div>
-        <img class="img-fluid" src="img/portfolio/${project.id}.png" alt="${project.name}">
-      </a>
-      <div class="portfolio-caption">
-        <h4>${project.name}</h4>
-        <p class="text-muted">${project.title}</p>
-        ${getProjLabels(project)}
       </div>
     </div>
   `)
